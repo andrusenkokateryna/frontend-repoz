78eeb251699d990a7aeec0f65821959f7d736839
Andrusenko Kateryna <andrusenkokateryna@gmail.com>
2018-12-10 00:15:10 +0200

    start

---
+++ good-look/js/script.js
@@ -0,0 +1,34 @@
+
+$(document).ready(function() {
+		$('.service-slide').slick({
+			arrows:false,
+			dots:true,
+			autoplay:false
+		});
+	});
+
+$(document).ready(function() {
+		$('.feedback').slick({
+			arrows:true,
+			dots:true,
+			autoplay:false
+		});
+	});
+
+var btn=document.querySelector('#show'),
+	menuMob = document.querySelector('.menu-for-small'),
+	close= document.querySelector('#close');
+
+	btn.addEventListener('click', function(){
+		menuMob.style.transform='translateX()';
+	})
+
+	close.addEventListener('click', function(){
+		menuMob.style.display='none';
+	})
+
+	window.addEventListener('click', function(e){
+		if(e.target==menuMob){
+			menuMob.style.display='none';
+		}
+	})
